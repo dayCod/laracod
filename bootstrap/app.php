391da186d83ffae67bc5b92a87fbe9876ba45e00
wirandra.rembon <alayarahman07@gmail.com>
2024-08-23 11:34:18 +0700

    Add (23/08/2024): Global and Versioned Api and Setup Pest Testing Laravel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -9,8 +10,14 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        then: function () {
+            Route::middleware('api')
+                ->prefix('api/v1')
+                ->group(base_path('routes/api/api_v1.php'));
+        }
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
